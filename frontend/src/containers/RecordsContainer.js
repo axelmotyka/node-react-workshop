ccfd58a3f42391eb91ccb4645023f2c8ed4c7ea8
Christian Pajung <christian.pajung@telekom.de>
2018-10-22 15:31:58 +0200

    add NewsCards

--- frontend/src/containers/RecordsContainer.js
+++ frontend/src/containers/RecordsContainer.js
@@ -2,22 +2,27 @@ import React, { Component } from 'react';
 import Grid from '@material-ui/core/Grid';
 import NewRecordComponent from '../components/NewRecordComponent';
 import RecordsTableComponent from '../components/RecordsTableComponent';
+import NewsCard from '../components/NewsCard';
 
 class RecordsContainer extends Component {
-	render() {
-		console.log('Rendering NewRecordContainer');
-		return (
-			<Grid
-				container
-				direction="column"
-				justify="center"
-				alignItems="flex-start"
-				spacing={16}>
-				<NewRecordComponent />
-				<RecordsTableComponent />
-			</Grid>
-		);
-	}
+  render() {
+    console.log('Rendering NewRecordContainer');
+    var myArray = [1, 2, 3, 4, 5];
+
+    return (
+      <Grid
+        container
+        direction="column"
+        justify="center"
+        alignItems="flex-start"
+        spacing={16}>
+        <NewRecordComponent />
+        <RecordsTableComponent />
+        {myArray.map(item =>
+          <NewsCard title="MYTITLE" />)}
+      </Grid>
+    );
+  }
 }
 
 export default RecordsContainer;
