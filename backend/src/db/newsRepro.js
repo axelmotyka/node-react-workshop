3ef17f6df9904200a66c15cba370373a5607c1c1
[flesch] <[friedl.lesch@telekom.de]>
2018-10-24 11:31:40 +0200

    create data folder and article.js added function insertTestArticle in newsRepro.js

--- backend/src/db/newsRepro.js
+++ backend/src/db/newsRepro.js
@@ -54,6 +54,13 @@ class NewsRepro {
 		const dropStmt = 'DROP TABLE IF EXISTS favourites';
 		return this.dao.run(dropStmt);
 	}
+
+	insertTestArticle() {
+		//const article= require('../data/article');
+		const instStmt = 'INSERT INTO artikel (artikelID, sourceID, sourceName, author, title, description, url, urlToImage, publishedAt, content, md5-Hash) VALUES (01,'news-com-au', 'News.com.au', '', 'In a uni initiation, these students bobbed for apples in urine and one died', 'STUDENTS bobbed for apples in a mixture of booze and urine as part of a drink-fuelled initiation ceremony where a student died, an inquest has heard.', 'https://www.news.com.au/lifestyle/real-life/news-life/teens-bobbed-for-apples-in-urine-at-boozy-initiation-ceremony/news-story/fb50ad108a2bf67ff2998eca06ed1ab6', '2018-10-24T08:11:53Z', 'STUDENTS bobbed for apples in a mixture of booze and urine as part of a drink-fuelled initiation ceremony where a student died, an inquest has heard. First-year Economics student Ed Farmer, 20, died after being found slumped in a corridor not breathing at the… [+1659 chars]', 'https://www.news.com.au/lifestyle/real-life/news-life/teens-bobbed-for-apples-in-urine-at-boozy-initiation-ceremony/news-story/fb50ad108a2bf67ff2998eca06ed1ab6/2018-10-24T08:11:53Z')';
+		return 
+		
+	}
 }
 
 module.exports = NewsRepro;
